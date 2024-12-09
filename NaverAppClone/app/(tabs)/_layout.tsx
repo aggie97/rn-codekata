af067cc4b5c603db79cf588ea233c64576c338ee
aggie97 <4ggie1997@gmail.com>
2024-12-09 21:44:16 +0900

    💄 style: add style to tab

--- NaverAppClone/app/(tabs)/_layout.tsx
+++ NaverAppClone/app/(tabs)/_layout.tsx
@@ -1,26 +1,38 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
 import {Tabs} from 'expo-router';
 import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 
+const HomeIcon = ({focused, color}: {focused: boolean; color: string}) => {
+  const iconName = focused ? 'home' : 'home-outline';
+  return <MaterialCommunityIcons name={iconName} color={color} size={26} />;
+};
+const ShoppingIcon = ({focused, color}: {focused: boolean; color: string}) => {
+  const iconName = focused ? 'shopping' : 'shopping-outline';
+  return <MaterialCommunityIcons name={iconName} color={color} size={26} />;
+};
+
 export default function TabLayout() {
   return (
-    <Tabs screenOptions={{tabBarActiveTintColor: 'blue'}}>
+    <Tabs
+      screenOptions={{
+        tabBarStyle: {
+          backgroundColor: 'black',
+        },
+        tabBarActiveTintColor: 'white',
+        tabBarInactiveTintColor: 'gray',
+        headerShown: false,
+      }}>
       <Tabs.Screen
         name="index"
         options={{
           title: 'Home',
-          tabBarIcon: ({color}) => (
-            <FontAwesome size={28} name="home" color={color} />
-          ),
+          tabBarIcon: HomeIcon,
         }}
       />
       <Tabs.Screen
         name="shopping"
         options={{
           title: 'Shopping',
-          tabBarIcon: ({color}) => (
-            <MaterialCommunityIcons name="shopping" size={24} color="black" />
-          ),
+          tabBarIcon: ShoppingIcon,
         }}
       />
     </Tabs>
