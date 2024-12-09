21917c17cb1ae4f392b41a88a1e832fcd4f3f77c
aggie97 <4ggie1997@gmail.com>
2024-12-09 21:44:54 +0900

    ✨ feat: add connection to naver & block additional access

--- NaverAppClone/app/(tabs)/index.tsx
+++ NaverAppClone/app/(tabs)/index.tsx
@@ -1,17 +1,37 @@
-import {View, Text, StyleSheet} from 'react-native';
-
-export default function Tab() {
-  return (
-    <View style={styles.container}>
-      <Text>Tab [Home|Shopping]</Text>
-    </View>
-  );
-}
+import {router} from 'expo-router';
+import {Platform, SafeAreaView, StatusBar, StyleSheet} from 'react-native';
+import WebView from 'react-native-webview';
 
 const styles = StyleSheet.create({
-  container: {
+  safeArea: {
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
   },
 });
+
+export default function HomeScreen() {
+  return (
+    <SafeAreaView style={styles.safeArea}>
+      <WebView
+        source={{uri: 'https://m.naver.com'}}
+        showsVerticalScrollIndicator={false}
+        showsHorizontalScrollIndicator={false}
+        onShouldStartLoadWithRequest={request => {
+          console.log('Home - Request', request);
+          if (
+            request.url.startsWith('https://m.naver.com') ||
+            request.mainDocumentURL?.startsWith('https://m.naver.com')
+          ) {
+            return true;
+          }
+
+          if (request.url != null && request.url.startsWith('https://')) {
+            router.navigate({pathname: 'browser'});
+            return false;
+          }
+          return true;
+        }}
+      />
+    </SafeAreaView>
+  );
+}
